ea72b5c68f32abd880297821c29297b1268fac3f
Dimitrie Tataru <dimitriettr@gmail.com>
2026-09-19 23:06:59 +0300

    Configure ESLint and Prettier

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,14 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules/**', 'build/**']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,4 +21,27 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 120,
+          tabWidth: 2,
+          tabs: false,
+          semicolons: true,
+          singleQuote: true,
+          bracketSpaces: true,
+          trailingComma: 'all',
+          arrowParens: 'avoid',
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+  prettier,
+]);
